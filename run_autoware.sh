efe715c4b12784eabf9fe91ae53b511194f59b0e
jasonlee0617 <676983492@qq.com>
2026-07-13 14:34:56 +0800

    2026 07.13 14：34

--- run_autoware.sh
+++ run_autoware.sh
@@ -6,7 +6,7 @@ source /opt/ros/humble/setup.bash
 source ~/autoware/install/setup.bash
 source ~/Wtbcar_autoware_nav2/install/setup.bash
 ros2 launch autoware_launch autoware.launch.xml \
-  map_path:=/home/eisa/autoware_map/maps/wtb_map4 \
+  map_path:=/home/eisa/autoware_map/maps/wtb_map \
   vehicle_model:=wtb_vehicle \
   sensor_model:=wtb_sensor_kit \
   data_path:=/home/eisa/autoware_data
